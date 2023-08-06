0bad22098d59ef3e056500b0ef0675308f6d2f4f
krr <kmrrhlrj.career@gmail.com>
2023-08-06 18:35:23 +0530

    whole project done

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        ProductServices productServices = new ProductServices();
+
+        productServices.addProduct(new Product("HP Laptop", "Wood Drawer", "Laptop", 2025));
+        productServices.addProduct(new Product("MacBook Air", "Study Table", "Laptop", 2024));
+        productServices.addProduct(new Product("Pixel 4a", "Bed", "Phone", 2023));
+        productServices.addProduct(new Product("Mivi Earpod", "Study Table", "Earphone", 2024));
+        productServices.addProduct(new Product("Smart Watch", "Wood Drawer", "Watch", 2022));
+        productServices.addProduct(new Product("iPhone SE", "Wood Drawer", "Phone", 2020));
+
+        Product p = productServices.getProductByName("HP");
+        System.out.println(p);
+
+        List<Product> apl = productServices.getAllProducts();
+        System.out.println(apl);
+
+        List<Product> pbt = productServices.getProductByText("Laptop");
+        System.out.println(pbt);
+
+        List<Product> pbp = productServices.getProductByPlace("Bed");
+        System.out.println(pbp);
+
+        List<Product> spow = productServices.getProductOutOfWarranty();
+        System.out.println(spow);
+    }
+}
---
+++ src/ProductServices.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductServices {
+
+    List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p){
+        products.add(p);
+    }
+
+    public List<Product> getAllProducts(){
+        return products;
+    }
+
+    public Product getProductByName(String str){
+        Product pTemp = null;
+        for(Product p : products){
+            if(p.getName().equals(str))
+                pTemp = p;
+        }
+        if(pTemp!=null)
+            return pTemp;
+        else
+            return new Product("x", "y", "z", 1);
+    }
+
+    public List<Product> getProductByText(String s){
+        List<Product> pTemp = new ArrayList<>();
+        String txt = s.toLowerCase();
+        for(Product p : products){
+            String name = p.getName().toLowerCase();
+            String place = p.getPlace().toLowerCase();
+            String type = p.getType().toLowerCase();
+            if(name.contains(txt) || place.contains(txt) || type.contains(txt)){
+                pTemp.add(p);
+            }
+        }
+        return pTemp;
+    }
+
+    public List<Product> getProductByPlace(String s){
+        List<Product> pTemp = new ArrayList<>();
+        String sPlace = s.toLowerCase();
+        for(Product p : products){
+            String place = p.getPlace().toLowerCase();
+            if(place.equals(sPlace))
+                pTemp.add(p);
+        }
+        return pTemp;
+    }
+
+    public List<Product> getProductOutOfWarranty(){
+        List<Product> pTemp = new ArrayList<>();
+        for(Product p : products){
+            if(p.getWarranty()>2023)
+                pTemp.add(p);
+        }
+        return pTemp;
+    }
+}
